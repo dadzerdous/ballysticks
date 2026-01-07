51a5358b9747efcfa0aebfe7f652432cd24deda2
dadzerdous <dadzaverous@gmail.com>
2026-01-07 06:36:55 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 export const Config = {
-    gravity: 0.38,        // LOWERED from 0.45 (Easier to aim)
+    gravity: 0.38,
     friction: 0.99,
     wallBounce: 0.7,
     jumpPower: 21,
-    wallSlideSpeed: 1.5,
-    aimSpeed: 0.05,
+    wallSlideSpeed: 0.3,  // CHANGED: Slowed way down (was 1.5)
+    aimSpeed: 0.04,       // CHANGED: Slightly slower aim for precision
     zones: [
         { h: 0, name: "The City", color: [20, 20, 20] },
-        { h: 50, name: "Skyline", color: [20, 20, 50] },     // Moving walls start here
+        { h: 50, name: "Skyline", color: [20, 20, 50] },
         { h: 150, name: "Atmosphere", color: [40, 20, 60] },
         { h: 300, name: "Stratosphere", color: [60, 10, 30] },
         { h: 500, name: "The Void", color: [0, 0, 0] }
