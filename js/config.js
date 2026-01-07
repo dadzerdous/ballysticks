e23259279b8a1eccbdc1d9ee71a5ba3972c78da6
dadzerdous <dadzaverous@gmail.com>
2026-01-07 02:03:52 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,18 +1,24 @@
-
 export const Config = {
-    gravity: 0.45,
-    friction: 0.99,       // Air resistance
-    wallBounce: 0.7,      // Bounciness of walls
+    gravity: 0.38,        // LOWERED from 0.45 (Easier to aim)
+    friction: 0.99,
+    wallBounce: 0.7,
     jumpPower: 21,
-    wallSlideSpeed: 1.5,  // Speed of sliding down walls
-    aimSpeed: 0.05,       // How fast the laser sweeps
+    wallSlideSpeed: 1.5,
+    aimSpeed: 0.05,
     zones: [
         { h: 0, name: "The City", color: [20, 20, 20] },
-        { h: 50, name: "Skyline", color: [20, 20, 50] },
+        { h: 50, name: "Skyline", color: [20, 20, 50] },     // Moving walls start here
         { h: 150, name: "Atmosphere", color: [40, 20, 60] },
         { h: 300, name: "Stratosphere", color: [60, 10, 30] },
         { h: 500, name: "The Void", color: [0, 0, 0] }
     ]
+};
+
+export const GameState = {
+    currency: parseInt(localStorage.getItem('canyon_currency')) || 0,
+    save: function() {
+        localStorage.setItem('canyon_currency', this.currency);
+    }
 };
 
 export const Utils = {
