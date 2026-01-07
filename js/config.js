be046dab071f787b68d3241ef3c3d8c9bcc2c348
dadzerdous <dadzaverous@gmail.com>
2026-01-07 13:33:31 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,12 +4,9 @@ export const Config = {
     wallBounce: 0.7,
     jumpPower: 21,
     aimSpeed: 0.04,
-    
-    // NEW GRIP SETTINGS
-    gripDuration: 90,     // Frames (approx 1.5 seconds at 60fps)
-    slipAcceleration: 0.1, // How fast the slipping speeds up
-    maxSlipSpeed: 8.0,     // Terminal velocity when slipping
-    
+    gripDuration: 90,
+    slipAcceleration: 0.1,
+    maxSlipSpeed: 8.0,
     zones: [
         { h: 0, name: "The City", color: [20, 20, 20] },
         { h: 50, name: "Skyline", color: [20, 20, 50] },
@@ -21,8 +18,13 @@ export const Config = {
 
 export const GameState = {
     currency: parseInt(localStorage.getItem('canyon_currency')) || 0,
+    activeColor: localStorage.getItem('ninja_color') || '#00ffcc',
+    unlockedColors: JSON.parse(localStorage.getItem('unlocked_colors')) || ['#00ffcc'],
+    
     save: function() {
         localStorage.setItem('canyon_currency', this.currency);
+        localStorage.setItem('ninja_color', this.activeColor);
+        localStorage.setItem('unlocked_colors', JSON.stringify(this.unlockedColors));
     }
 };
 
